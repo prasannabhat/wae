e28dbc6d9e967c83cda055aa5a739c1c224b184e
prasannabhat <prasanna.yoga@gmail.com>
2012-08-13 08:56:09 +0530

    Add import module in python

---
+++ utilities/python_import.py
@@ -0,0 +1,6 @@
+import string_test
+
+test = "hello world"
+print test
+test = string_test.replacer(test,['e','l','o'],'?')
+print test
--- utilities/string_test.py
+++ utilities/string_test.py
@@ -2,7 +2,3 @@ def replacer(str,items,repl):
     x = "".join(map(lambda x:repl if x in items else x,str))
     return x
 
-test = "hello world"
-print test
-test = replacer(test,['e','l','o'],'?')
-print test
